2285d8f9dbdfeee6739dc6bec4e842fb5ce4de78
Bob Van Valzah <Bob@VanValzah.Com>
2025-09-04 21:08:58 -0500

    Pulled TIC code out into its own file.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,17 @@
 # gpsdo_main.py
 import asyncio
-import contextlib
 import json
 import logging
-import re
-import termios
 import time
 from dataclasses import dataclass
 from enum import Enum
 
-import serial_asyncio as serialAsyncio
-
 from f9t import F9T
-from quietWatch import QuietWatch
+from tic import TIC
 
 # third party imports (install as needed)
 # import aiosqlite
 
-# XXX next up: Move TIC open code to a class like F9T
-# XXX next up: Time-bounded consideration of watchdog for F9T and TIC
 # XXX next up: Initialize F9T to output TIM-TP messages and other required state
 # XXX next up: Figure out what @dataclass does
 # XXX next up: Define mapping of chA/B to PPS signals
@@ -50,91 +43,6 @@ class Registry:
         self.dacCode = 0
         self.lastPpsErrorNs = None
         self.health = {"sat": 0, "f9tOk": False, "ticOk": False}
-
-
-
-class TicState(Enum): # XXX This should be a state variable for debug inspection
-    startup = 0 # We are ready to send a character to get the config menu
-    config1 = 1 # Have sent character, waiting for the "choose one:" prompt
-    config2 = 2 # Have sent reset, waiting for 2nd "choose one:" prompt
-    stamping = 3 # Have written config changes and are now timestamping
-
-async def ticReader(eventBus, port, baud, discard_interval=1.0):
-    reader, writer = await serialAsyncio.open_serial_connection(url=port, baudrate=baud)
-    transport = writer.transport
-
-    # If the last guy to open the TIC's serial port didn't set HUPCL, we may see
-    # garbeled output at first, then timestamps continuing from the previous run.
-    # To avoid that when we're done, we want to leave it in a reset state for our
-    # next open, so we set HUPCL if it's not set.
-    #
-    # Pause callbacks while we tweak termios
-    transport.pause_reading()
-    try:
-        serialObj = (transport.get_extra_info("serial")
-                  or getattr(transport, "serial", None))
-        fd = serialObj.fileno()
-        attrs = termios.tcgetattr(fd)
-        cflag = attrs[2]
-        if not cflag & termios.HUPCL:
-            # HUPCL was not set, so set it now and re-open the serial port
-            cflag |= termios.HUPCL
-            attrs[2] = cflag
-            termios.tcsetattr(fd, termios.TCSANOW, attrs)
-            writer.close()
-            with contextlib.suppress(Exception):
-                await writer.wait_closed()   # Python 3.8+; waits for transport shutdown
-            await asyncio.sleep(0.1)
-            # Re-open serial connection, now with HUPCL for next time we open it
-            reader, writer = await serialAsyncio.open_serial_connection(url=port,
-                baudrate=baud)
-
-    finally:
-        transport.resume_reading()
-
-    # Opening the port this way means the TIC is always in its startup state.
-    # We run through the config menu to quickly reset it to all defaults and start
-    # timestamping quickly.
-    ticState = TicState.startup
-
-    dog = QuietWatch(name=port, warnAfterSec=10)
-    dogTask = asyncio.create_task(dog.run())
-
-    try:
-        while True:
-            raw = await reader.readline()
-            # XXX this would be the spot to catch SerialException when TIC is unplugged
-            line = raw.decode("utf-8").rstrip()
-            if not line:
-                continue
-            if (ticState == TicState.startup
-            and re.fullmatch(r"# Type any character for config menu", line)):
-                writer.write(b'x')
-                ticState = TicState.config1
-            if re.fullmatch(r"choose one:", line):
-                if ticState==TicState.config1:
-                    writer.write(b'r')
-                    ticState = TicState.config2
-                elif ticState==TicState.config2:
-                    writer.write(b'w')
-                    ticState = TicState.stamping
-            if ticState!=TicState.stamping:
-                continue
-
-            # Process lines after configured and stamping
-            if not re.fullmatch(r"\d+\.\d{12} ch[AB]", line):
-                # XXX log stats here
-                # print("ignoring TIC line", line)
-                continue  # Ignore the line if it doesn't match a timestamp
-            dog.pet()
-            print("got TIC data", line)
-            sample = {"ppsErrorNs": 123}  # placeholder
-            # await eventBus.put(Event(nowNs(), "tic", "ppsSample", sample))
-            # await asyncio.sleep(1.0)
-    finally:
-        dog.stop()
-        await dogTask
-        pass
 
 
 async def dacActor(cmdQueue):
@@ -226,9 +134,10 @@ async def main():
 
     dacQueue = asyncio.Queue()
     f9t = F9T(eventBus, "/dev/ttyACM1", 9600)
+    tic = TIC(eventBus, "/dev/ttyACM0", 115200)
     tasks = [
-        asyncio.create_task(f9t.runF9tStream()),
-        asyncio.create_task(ticReader(eventBus, "/dev/ttyACM0", 115200)),
+        asyncio.create_task(f9t.run()),
+        asyncio.create_task(tic.run()),
         asyncio.create_task(controlLoop(eventBus, reg, dacQueue)),
         asyncio.create_task(dacActor(dacQueue)),
         asyncio.create_task(storageWriter(storageTap)),
